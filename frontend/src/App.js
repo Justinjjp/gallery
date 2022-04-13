432482d1f32e98949d1dc446702010acd0519068
Justin <justinjjp006@gmail.com>
2022-04-13 17:11:43 +0530

    First-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href ="/">Galllery</a>
+      </header>
+      <main>
+        List Art Galllery
+      </main>
+    </div>
+  );
+}
+
+export default App;
