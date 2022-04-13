70c92d52e0e9d99cef04dc0d6d69ff51a2399887
Justin <justinjjp006@gmail.com>
2022-04-13 17:53:02 +0530

    map fun..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from "./data";
+
 function App() {
   return (
     <div className="App">
@@ -5,7 +7,23 @@ function App() {
         <a href ="/">Galllery</a>
       </header>
       <main>
-        List Art Galllery
+        <h1>Featured Arts</h1>
+        <div className="products">
+        {
+          data.products.map(product => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+            </a>
+            <div className="productInfo">
+            <a href={`/product/${product.slug}`}>
+            <p>{product.name}</p>
+            </a>
+            <p><strong>${product.price}</strong> </p>
+            <button>Add to Cart</button>
+            </div>
+          </div>)) }
+        </div>
       </main>
     </div>
   );
