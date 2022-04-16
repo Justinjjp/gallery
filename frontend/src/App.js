12385ed61f9ca2d76155b072b49e9e1742645b13
Justin <justinjjp006@gmail.com>
2022-04-16 09:51:15 +0530

    Adding reactBootStrap for the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import HomeScreen from "./Screens/HomeScreen";
 import ProductScreen from "./Screens/ProductScreen";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
+
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
+    <div className="d-flex flex-column site-container">
       <header>
-        <Link to ="/">Galllery</Link>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+            <Navbar.Brand>Galllery</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
       <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
+        <Container>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </Container>
       </main>
+      <footer>
+        <div className="text-center">All rights reserved @ Justin P</div>
+      </footer>
     </div>
     </BrowserRouter>
   );
